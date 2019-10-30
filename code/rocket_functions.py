378d410bd8c0849652731abe7fc13e61804cd46a
Thomas Capelle <thomascapelle@gmail.com>
2019-10-30 14:54:20 +0100

    added multichannel support

--- code/rocket_functions.py
+++ code/rocket_functions.py
@@ -1,21 +1,17 @@
-# Angus Dempster, Francois Petitjean, Geoff Webb
-
-# Dempster A, Petitjean F, Webb GI (2019) ROCKET: Exceptionally fast and
-# accurate time series classification using random convolutional kernels.
-# arXiv:1910.13051
+#A multichannel version of Rocket
 
 from numba import njit, prange
 import numpy as np
 
-@njit
-def generate_kernels(input_length, num_kernels):
-
+# @njit
+def generate_kernels(input_shape, num_kernels, dtype=np.float32):
+    channels, input_length = input_shape
     candidate_lengths = np.array((7, 9, 11))
 
     # initialise kernel parameters
-    weights = np.zeros((num_kernels, candidate_lengths.max())) # see note
+    weights = np.zeros((num_kernels, channels, candidate_lengths.max()), dtype = dtype) # see note
     lengths = np.zeros(num_kernels, dtype = np.int32) # see note
-    biases = np.zeros(num_kernels)
+    biases = np.zeros(num_kernels, dtype = dtype)
     dilations = np.zeros(num_kernels, dtype = np.int32)
     paddings = np.zeros(num_kernels, dtype = np.int32)
 
@@ -24,30 +20,30 @@ def generate_kernels(input_length, num_kernels):
     for i in range(num_kernels):
 
         length = np.random.choice(candidate_lengths)
-        _weights = np.random.normal(0, 1, length)
-        bias = np.random.uniform(-1, 1)
+        _weights = np.random.normal(0, 1, (channels, length))
+        bias = np.random.uniform(-1, 1, 1)
         dilation = 2 ** np.random.uniform(0, np.log2((input_length - 1) // (length - 1)))
         padding = ((length - 1) * dilation) // 2 if np.random.randint(2) == 1 else 0
-
-        weights[i, :length] = _weights - _weights.mean()
+        weights[i,:,:length] = _weights - _weights.mean(axis=1, keepdims=True)
         lengths[i], biases[i], dilations[i], paddings[i] = length, bias, dilation, padding
 
     return weights, lengths, biases, dilations, paddings
 
+
 @njit(fastmath = True)
 def apply_kernel(X, weights, length, bias, dilation, padding):
-
+    # print(f'X: {X.shape}')
+    # print(f'w: {weights.shape}')
+    channels, input_length = X.shape
     # zero padding
     if padding > 0:
-        _input_length = len(X)
-        _X = np.zeros(_input_length + (2 * padding))
-        _X[padding:(padding + _input_length)] = X
+        _X = np.zeros((channels, input_length + (2 * padding)), dtype=np.float32)
+        _X[:,padding:(padding + input_length)] = X
         X = _X
 
-    input_length = len(X)
+    _, input_length = X.shape
 
     output_length = input_length - ((length - 1) * dilation)
-
     _ppv = 0 # "proportion of positive values"
     _max = np.NINF
 
@@ -56,8 +52,8 @@ def apply_kernel(X, weights, length, bias, dilation, padding):
         _sum = bias
 
         for j in range(length):
-
-            _sum += weights[j] * X[i + (j * dilation)]
+            for c in range(channels):
+                _sum += weights[c, j] * X[c, i + (j * dilation)]
 
         if _sum > 0:
             _ppv += 1
@@ -76,11 +72,11 @@ def apply_kernels(X, kernels):
     num_kernels = len(weights)
 
     # initialise output
-    _X = np.zeros((num_examples, num_kernels * 2)) # 2 features per kernel
+    _X = np.zeros((num_examples, num_kernels * 2), dtype=np.float32) # 2 features per kernel
 
-    for i in prange(num_examples):
+    for i in range(num_examples):
 
-        for j in range(num_kernels):
+        for j in prange(num_kernels):
 
             _X[i, (j * 2):((j * 2) + 2)] = \
             apply_kernel(X[i], weights[j][:lengths[j]], lengths[j], biases[j], dilations[j], paddings[j])
